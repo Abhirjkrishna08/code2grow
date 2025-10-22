443d526dafa1d57e26853d3107c3c76b52d0f50f
Abhiraj Krishna <abhiraj_r@srmap.edu.in>
2025-10-22 21:26:16 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+    float basic_salary, hra_percent, da_percent, tax_percent,hra, da, tax, gross_salary, net_monthly;
+    printf("Enter basic salary");
+    scanf("%f", &basic_salary);
+    printf("Enter hra ");
+    scanf("%f", &hra_percent);
+    printf("Enter DA ");
+    scanf("%f", &da_percent); 
+    printf("Enter tax ");
+    scanf("%f", &tax_percent);
+    hra = basic_salary * (hra_percent / 100);
+    da = basic_salary * (da_percent / 100);
+    tax = basic_salary * (tax_percent / 100);
+    gross_salary = basic_salary + hra + da - tax;
+
+    printf("HRA = %f\n", hra);
+    printf("DA = %f\n", da);
+    printf("TAX = %f\n", tax);
+    printf("Gross Salary = %f\n", gross_salary);
+    printf("Is Gross Salary above 50000 %d\n", gross_salary > 50000);
+    net_monthly = gross_salary/12;
+    printf("Net Monthly Salary = %f\n", net_monthly);
+    return 0;
+}
